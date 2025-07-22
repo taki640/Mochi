7cbbeddbba56bd50e5a73c3507a5a9f3adae05c7
Taki Takanashi <gustavo.almeidafn@gmail.com>
2025-07-22 10:09:46 -0300

    Added scoped ImGui style

--- Mochi/Include/Mochi/ImGuiScoped.hpp
+++ Mochi/Include/Mochi/ImGuiScoped.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "../../Source/Base.hpp"
+
+struct ImVec2;
+struct ImVec4;
+
 namespace Mochi
 {
 	class ScopedImGuiID
@@ -7,5 +12,19 @@ namespace Mochi
 	public:
 		ScopedImGuiID(int id);
 		~ScopedImGuiID();
+	};
+
+	class ScopedImGuiStyle
+	{
+	public:
+		~ScopedImGuiStyle();
+
+		void PushStyleVar(int styleVar, float value);
+		void PushStyleVar(int styleVar, const ImVec2& value);
+		void PushStyleColor(int color, uint32_t value);
+		void PushStyleColor(int color, const ImVec4& value);
+	private:
+		int m_StyleCount = 0;
+		int m_ColorCount = 0;
 	};
 }
--- Mochi/Source/ImGuiScoped.cpp
+++ Mochi/Source/ImGuiScoped.cpp
@@ -13,4 +13,34 @@ namespace Mochi
 	{
 		ImGui::PopID();
 	}
+
+	ScopedImGuiStyle::~ScopedImGuiStyle()
+	{
+		ImGui::PopStyleColor(m_ColorCount);
+		ImGui::PopStyleVar(m_StyleCount);
+	}
+
+	void ScopedImGuiStyle::PushStyleVar(ImGuiStyleVar styleVar, float value)
+	{
+		ImGui::PushStyleVar(styleVar, value);
+		m_StyleCount++;
+	}
+
+	void ScopedImGuiStyle::PushStyleVar(ImGuiStyleVar styleVar, const ImVec2& value)
+	{
+		ImGui::PushStyleVar(styleVar, value);
+		m_StyleCount++;
+	}
+
+	void ScopedImGuiStyle::PushStyleColor(ImGuiCol color, ImU32 value)
+	{
+		ImGui::PushStyleColor(color, value);
+		m_ColorCount++;
+	}
+
+	void ScopedImGuiStyle::PushStyleColor(ImGuiCol color, const ImVec4& value)
+	{
+		ImGui::PushStyleColor(color, value);
+		m_ColorCount++;
+	}
 }
